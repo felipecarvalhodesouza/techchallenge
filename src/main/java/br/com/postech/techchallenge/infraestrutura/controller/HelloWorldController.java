4a4496cc5ebd5131d7ed111161b964d95496ab71
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2024-05-13 22:12:12 -0300

    Correção endpoint

--- src/main/java/br/com/postech/techchallenge/infraestrutura/controller/HelloWorldController.java
+++ src/main/java/br/com/postech/techchallenge/infraestrutura/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	@ApiResponse(responseCode = "200")
